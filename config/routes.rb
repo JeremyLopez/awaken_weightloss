51ca92aa67e49a55775ed47d6d94732210ebbda7
jeremyy7 <jeremyy7@gmail.com>
2015-09-12 13:02:32 +0000

    did some navbar work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  root to: "static_pages#home"
+  
+  
+  get '/help' => 'static_pages#help' 
+  get '/about' => 'static_pages#about'
 end
